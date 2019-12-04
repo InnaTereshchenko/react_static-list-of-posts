d4355fdf2dfe329a599d54181b4c5f1be64b8339
Inna <inna.tereshchenko1@gmail.com>
2019-12-04 17:25:47 +0200

    Updates

--- precache-manifest.70fa819c296674e41728f1d20c503fd5.js
+++ precache-manifest.70fa819c296674e41728f1d20c503fd5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ddcc2d6851450582f344093a1fee4ec6",
+    "revision": "53c50bc08a0ee8f1d3ad129ff3ff1f30",
     "url": "/react_static-list-of-posts/index.html"
   },
   {
-    "revision": "f73cda99b3a913136ce3",
-    "url": "/react_static-list-of-posts/static/css/main.a4184be1.chunk.css"
+    "revision": "e98ce8af67958732582f",
+    "url": "/react_static-list-of-posts/static/css/main.2ee596aa.chunk.css"
   },
   {
     "revision": "d1bf606ea68b30240b50",
     "url": "/react_static-list-of-posts/static/js/2.a7975bab.chunk.js"
   },
   {
-    "revision": "f73cda99b3a913136ce3",
-    "url": "/react_static-list-of-posts/static/js/main.ebbc4049.chunk.js"
+    "revision": "e98ce8af67958732582f",
+    "url": "/react_static-list-of-posts/static/js/main.e08708c5.chunk.js"
   },
   {
     "revision": "3fdea9cdc8e2a1e9a77e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_static-list-of-posts/precache-manifest.2002ba282a513466ce6596c8921e0f83.js"
+  "/react_static-list-of-posts/precache-manifest.70fa819c296674e41728f1d20c503fd5.js"
 );
 
 self.addEventListener('message', (event) => {
